274186df087e4dee95ae5093e49274037f843378
user <bobrovdanel@gmail.com>
2024-04-15 18:59:59 +0300

    add tg bot

---
+++ bot/database.py
@@ -0,0 +1,87 @@
+import sqlite3
+
+def create_database(name):
+    db = sqlite3.connect(name)
+    db.row_factory = sqlite3.Row
+    with open("db_struct.sql", mode="r") as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+
+class DatabaseError(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = "\n".join([str(i) for i in args])
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return self.message
+        else:
+            return "DatabaseError has been raised"
+
+
+class Database:
+    def __init__(self, database=None):
+        if database is None:
+            database = sqlite3.connect("database.sqlite")
+            database.row_factory = sqlite3.Row
+        self.__db = database
+        self.__cur = database.cursor()
+
+    # members ------------------------------------------------------------------------------------------------------
+
+    def get_user(self, telegram_id):
+        try:
+            self.__cur.execute(f"SELECT * FROM users WHERE telegram_id = ?",
+                               (telegram_id,))
+            step = self.__cur.fetchall()
+            if not len(step):
+                self.__add_user(telegram_id)
+                return "step1"
+            return step[0]["step"]
+        except Exception as exception:
+            raise DatabaseError("Can't get user from database.", exception)
+
+    def update_step(self, telegram_id, step):
+        try:
+            self.__cur.execute("UPDATE users SET step = ? WHERE telegram_id = ?", (step, telegram_id))
+            self.__db.commit()
+        except Exception as exception:
+            raise DatabaseError("Can't update user's step.", exception)
+
+    def get_photo(self, telegram_id):
+        try:
+            self.__cur.execute("SELECT photo FROM users WHERE telegram_id = ?", (telegram_id,))
+            photo = self.__cur.fetchall()
+            if not len(photo):
+                return None
+            return photo[0]["photo"]
+        except Exception as exception:
+            raise DatabaseError("Can't add user to database.", exception)
+
+    def add_photo(self, telegram_id, path):
+        try:
+            self.__cur.execute("UPDATE users SET photo = ? WHERE telegram_id = ?", (path, telegram_id))
+            self.__db.commit()
+        except Exception as exception:
+            raise DatabaseError("Can't add user to database.", exception)
+
+    def __add_user(self, telegram_id):
+        try:
+            self.__cur.execute(f"INSERT INTO users VALUES (NULL, ?, '', 'step1')",
+                               (telegram_id,))
+            self.__db.commit()
+        except Exception as exception:
+            raise DatabaseError("Can't add user to database.", exception)
+
+
+if __name__ == "__main__":
+    # create_database("database.db")
+    db = Database()
+    db.get_user(2)
+    print(db.get_user(2))
+    db.add_photo(2, "path/to/photo.png")
+    print(db.get_photo(2))
---
+++ bot/main.py
@@ -0,0 +1,239 @@
+import asyncio
+
+import telebot
+from telebot import types
+from telebot.async_telebot import AsyncTeleBot
+
+import database
+
+
+TOKEN = "<your token>"
+
+bot = AsyncTeleBot(token=TOKEN)
+db = database.Database()
+
+
+def validate(message: telebot.types.Message, expected):
+    """
+    Checks if the message is valid.
+    :param message: message to be checked
+    :param expected: "text", "photo", "document" or "callback"
+    :return: bool, True if the message is valid, otherwise False
+    """
+    if type(message) is telebot.types.CallbackQuery:
+        return expected == "callback"
+    return expected == message.content_type
+
+
+async def step1(message):
+    if not validate(message, "text"):
+        await bot.send_message(message.message.chat.id, "Отправь текстовое сообщение")
+        return step1.__name__
+    validate(message, "text")
+    markup = types.InlineKeyboardMarkup().add(types.InlineKeyboardButton("Загрузить фото 🐅", callback_data="step_1"))
+    await bot.send_message(message.chat.id, "Привет, я ТигроБот! 🐯 Я умный тигр и я могу определить, на кого из моих сородичей ты похож. Чтобы узнать ответ, нажми на кнопку и загрузи свое фото!", reply_markup=markup)
+    return step2.__name__
+
+
+async def step2(callback):
+    if not validate(callback, "callback"):
+        await bot.send_message(callback.message.chat.id, "Нажми на кнопку")
+        return step2.__name__
+    await bot.send_message(callback.message.chat.id, "Жду твое фото!")
+    return step3.__name__
+
+
+async def step3(message):
+    if not validate(message, "photo"):
+        try:
+            await bot.send_message(message.chat.id, "Отправь фото")
+            return step3.__name__
+        except Exception as e:
+            await bot.send_message(message.message.chat.id, "Отправь фото")
+            return step3.__name__
+    photo = message.photo
+    await bot.send_message(message.chat.id, "Кажется, я знаю, кто ты 😏")
+
+    file_id = photo[-1].file_id
+    file_info = await bot.get_file(file_id)
+    photo = await bot.download_file(file_info.file_path)
+
+    with open(f"photos/{file_id}.png", "wb") as f:
+        f.write(photo)
+
+        db.add_photo(message.chat.id, f"photos/{file_id}.png")
+
+    ''' тут надо фотку обработать '''
+
+    await bot.send_photo(message.chat.id, photo, caption="<description>")
+
+    buttons = types.InlineKeyboardMarkup().add(types.InlineKeyboardButton("Да", callback_data="yes"))
+    buttons.add(types.InlineKeyboardButton("А что это?", callback_data="question"))
+    buttons.add(types.InlineKeyboardButton("Нет", callback_data="no"))
+    await bot.send_message(message.chat.id, "Хочешь микс с Кандинским? 🧑🏻‍ 🪄 🐯", reply_markup=buttons)
+
+    return step4.__name__
+
+
+async def end(callback):
+    if not validate(callback, "callback"):
+        return end.__name__
+    return await step8(callback.message)
+
+
+async def step6(message):
+    button = types.InlineKeyboardMarkup().add(types.InlineKeyboardButton("А что ты еще умеешь?", callback_data="step8"))
+    await bot.send_message(message.chat.id, "Мои малыши-помощники тигрята удивлены! 🐯😮 Но не переживай, может быть в следующий раз тебе захочется создать что-то особенное с нами! 🐾🎨", reply_markup=button)
+    return end.__name__
+
+
+async def step7(message, photo):
+    with open(photo, "rb") as f:
+        photo = f.read()
+    """ Делаем микс """
+    button = types.InlineKeyboardMarkup().add(types.InlineKeyboardButton("А что ты еще умеешь?", callback_data="step8"))
+    await bot.send_photo(message.chat.id, photo, caption="Вот, что у нас получилось✨😱", reply_markup=button)
+    return end.__name__
+
+
+async def step5(callback):
+    if not validate(callback, "callback"):
+        await bot.send_message(callback.chat.id, "Вернись к последнему сообщению и выбери нужную кнопку!")
+        return step5.__name__
+    answer = callback.data
+    if answer == "mix":
+        photo = db.get_photo(callback.from_user.id)
+        step = await step7(callback.message, photo)
+        return step
+    if answer == "not_mix":
+        step = await step6(callback.message)
+        return step
+    await bot.send_message(callback.message.chat.id, "Ой! Кажется, ты нажал не на ту кнопку. Вернись к последнему сообщению и выбери правильный вариант!")
+    return step5.__name__
+
+
+async def step4(callback):
+    if not validate(callback, "callback"):
+        await bot.send_message(callback.chat.id, "Нажми на кнопку с твоим вариантом ответа")
+        return step4.__name__
+    answer = callback.data
+    match answer:
+        case "yes":
+            photo = db.get_photo(callback.from_user.id)
+            ''' микс фотки '''
+            step = await step7(callback.message, photo)
+            return step
+        case "question":
+            buttons = types.InlineKeyboardMarkup().add(types.InlineKeyboardButton("Давай! 💪🎨", callback_data="mix"))
+            buttons.add(types.InlineKeyboardButton("Я передумал", callback_data="not_mix"))
+            await bot.send_message(callback.message.chat.id, "Кандинский - это такая умная штука - нейросеть, созданная разработчиками из Сбера. 🧠💡 Он может соединить твое фото с фото твоего тигрового двойника 🐯🔗 Интересно же, правда? 🤩 Готов попробовать? 😄", reply_markup=buttons)
+            return step5.__name__
+        case "no":
+            step = await step6(callback.message)
+            return step
+    await bot.send_message(callback.message.chat.id, "Ой. Ты нажал не на ту кнопку( вернись к последнему сообщению и выбери свой вариант ответа!")
+    return step4.__name__
+
+
+async def step8(message):
+    buttons = types.InlineKeyboardMarkup()
+    buttons.add(types.InlineKeyboardButton("Хочу еще разок 🤩", callback_data="restart"))
+    buttons.add(types.InlineKeyboardButton("Хочу поделиться ботом", switch_inline_query="Узнай, на какого тигра ты больше всего похож в этом боте! Присоединяйся и отправь свою фотографию, чтобы найти своего тигра-двойника! 📸🐯"))
+    buttons.add(types.InlineKeyboardButton("Хочу поделиться результатом", callback_data="share_result"))
+    buttons.add(types.InlineKeyboardButton("Хочу оставить отзыв 📝", callback_data="feedback"))
+    buttons.add(types.InlineKeyboardButton("Пока хватит", callback_data="finish"))
+    await bot.send_message(message.chat.id, "А что ты хочешь? 😁", reply_markup=buttons)
+    return step9.__name__
+
+
+async def another_step8(message):
+    buttons = types.InlineKeyboardMarkup()
+    buttons.add(types.InlineKeyboardButton("Хочу еще разок 🤩", callback_data="restart"))
+    buttons.add(types.InlineKeyboardButton("Хочу поделиться ботом", switch_inline_query="Узнай, на какого тигра ты больше всего похож в этом боте! Присоединяйся и отправь свою фотографию, чтобы найти своего тигра-двойника! 📸🐯"))
+    buttons.add(types.InlineKeyboardButton("Хочу поделиться результатом", callback_data="share_result"))
+    buttons.add(types.InlineKeyboardButton("Хочу оставить отзыв 📝", callback_data="feedback"))
+    buttons.add(types.InlineKeyboardButton("Пока хватит", callback_data="finish"))
+    await bot.send_message(message.chat.id, "Хочешь сделать что нибудь еще 🐯?", reply_markup=buttons)
+    return step9.__name__
+
+
+async def step9(callback):
+    if not validate(callback, "callback"):
+        await bot.send_message(callback.message.chat.id, "Нажми на кнопку с твоим вариантом ответа")
+        return step9.__name__
+    answer = callback.data
+    match answer:
+        case "restart":
+            await bot.send_message(callback.message.chat.id, "Жду твое фото!")
+            return step3.__name__
+        case "share_result":
+            await bot.send_message(callback.message.chat.id, "url")
+            await bot.send_message(callback.message.chat.id, "Ты можешь снова загрузить фото")
+            return step3.__name__
+        case "feedback":
+            buttons = types.InlineKeyboardMarkup().add(types.InlineKeyboardButton("Я передумал", callback_data="cancel"))
+            await bot.send_message(callback.message.chat.id, "Напиши свой отзыв в сообщении и отправь его мне 😊✍️", reply_markup=buttons)
+            return feedback.__name__
+        case "finish":
+            buttons = types.InlineKeyboardMarkup().add(types.InlineKeyboardButton("Тебе спасибо! Ты классный ❤️", callback_data="absolutly_finish"))
+            await bot.send_message(callback.message.chat.id, "Тогда спасибо, что заглянул поболтать со мной! 🐾  А то мне бывает скучно одному 🥺 Надеюсь, тебе было интересно со мной общаться! Буду рад видеть тебя здесь снова 🐯🧡", reply_markup=buttons)
+            return finish.__name__
+    await bot.send_message(callback.message.chat.id, "Ой. Ты нажал не на ту кнопку( вернись к последнему сообщению и выбери свой вариант ответа!")
+    return step9.__name__
+
+
+async def finish(callback):
+    if not validate(callback, "callback"):
+        await bot.send_message(callback.chat.id, "Нажми на кнопку с твоим вариантом ответа")
+        return finish.__name__
+    answer = callback.data
+    if answer == "absolutly_finish":
+        await bot.send_message(callback.message.chat.id, "Приходи в гости! Я буду тебя ждать 🤗🌿")
+        return step1.__name__
+    await bot.send_message(callback.message.chat.id, "Ой. Ты нажал не на ту кнопку( вернись к последнему сообщению и выбери свой вариант ответа!")
+    return finish.__name__
+
+
+async def feedback(message):
+    if validate(message, "callback"):
+        await bot.send_message(message.message.chat.id, "Очень жаль, ведь твоё мнение очень важно для меня🥺")
+        return await step8(message.message)
+    if not validate(message, "text"):
+        await bot.send_message(message.chat.id, "Отправь отзыв текстовым сообщением!")
+        return feedback.__name__
+    message: types.Message = message
+    print("\n"*5, "-"*100, "У нас есть отзыв!\n", message.text, "\n", "-"*100, "\n"*5)
+    await bot.send_message(message.chat.id, "Спасибо🙏🏻 Я всё передам, твоё мнение правда важно для нас 🐯❤️")
+    return await step8(message)
+
+
+@bot.message_handler(commands=["reset"])
+async def reset(message):
+    db.update_step(message.from_user.id, step1.__name__)
+
+
+@bot.message_handler(commands=["test"])
+async def test(message):
+    me = await bot.get_me()
+    print(me.id)
+    buttons = types.InlineKeyboardMarkup().add(
+        types.InlineKeyboardButton("test", switch_inline_query="Попробуй этого крутейшего бота!"))
+    await bot.send_message(message.chat.id, "text", reply_markup=buttons)
+
+
+@bot.message_handler(content_types=["text", "photo", "document"])
+@bot.callback_query_handler(func=lambda message: True)
+async def send_welcome(message):
+    step = db.get_user(message.from_user.id)
+    step = await globals()[step](message)
+    db.update_step(message.from_user.id, step)
+
+
+async def main():
+    await bot.infinity_polling(timeout=1000)
+
+
+if __name__ == '__main__':
+    print("Bot starting")
+    asyncio.run(globals()[main.__name__]())
+    print("Bot stopped")
